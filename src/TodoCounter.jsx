015708a890a8555a520ec2c19b3e02ac3a07329d
b7c8p <morapaezc@gmail.com>
2021-11-17 18:47:32 -0500

    start of styles

---
+++ src/TodoCounter.jsx
@@ -0,0 +1,10 @@
+function TodoCounter() {
+  return (
+    <header>
+      <h1>TASKS</h1>
+      <h2>Has completado 2 de 3 TODos</h2>
+    </header>
+  );
+}
+
+export { TodoCounter };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const e = React.createElement;
 
 ReactDOM.render(
     <App />,
