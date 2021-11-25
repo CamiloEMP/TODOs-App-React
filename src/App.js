117ab97f69a737195bcb7c0bc3121add7bc8aa58
b7c8p <morapaezc@gmail.com>
2021-11-24 20:25:54 -0500

    useEffect - createContext - localStorage

--- src/App.js
+++ src/App.js
@@ -1,77 +1,10 @@
-import { useState } from "react";
 import { AppUI } from "./AppUI";
-
-const defaultTodos = [
-  {
-    text: "Cortar pizza",
-    complete: true,
-  },
-  {
-    text: "Comer pizza",
-    complete: false,
-  },
-  {
-    text: "Tomar gaseosa",
-    complete: false,
-  },
-  {
-    text: "Tos",
-    complete: false,
-  },
-  {
-    text: "Tomaasdsa",
-    complete: false,
-  },
-];
-
+import { TodoProvider } from "./components/TodoContext";
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
-  const [searchValue, setSearchValue] = useState("");
-  const completedTodos = todos.filter((todo) => !!todo.complete).length;
-  const totalTodos = todos.length;
-  let searchedTodos = [];
-  if (!searchValue.length > 0) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter((todo) => {
-      const todoText = todo.text.toLocaleLowerCase();
-      const searchText = searchValue.toLocaleLowerCase();
-      return todoText.includes(searchText);
-    });
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos];
-    newTodos[todoIndex].complete = true;
-    setTodos(newTodos);
-    //Otra forma todos[todoIndex] = {
-    //   text: todos[todoIndex].text,
-    //   complete: true
-    // };
-  };
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
-    //Otra forma todos[todoIndex] = {
-    //   text: todos[todoIndex].text,
-    //   complete: true
-    // };
-  };
-
   return (
-    <AppUI
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
--- src/AppUI.js
+++ src/AppUI.js
@@ -1,3 +1,4 @@
+import { TodoContext } from "./components/TodoContext";
 import { HeaderNav } from "./components/HeaderNav";
 import { TodoCounter } from "./components/TodoCounter";
 import { TodoSearch } from "./components/TodoSearch";
@@ -5,31 +6,33 @@ import { TodoList } from "./components/TodoList";
 import { TodoItem } from "./components/TodoItem";
 import { CreateTodoButton } from "./components/CreateTodoButton";
 
-function AppUI({
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI() {
   return (
     <>
       <HeaderNav />
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.complete}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoContext.Consumer>
+        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
+          <TodoList>
+            {error && <p style={{ color: "#fff" }}>ERROR</p>}
+            {loading && <p style={{ color: "#fff" }}>Cargando</p>}
+            {!loading && !searchedTodos.length && (
+              <p style={{ color: "#fff" }}>Crea tu primer TODO</p>
+            )}
+            {searchedTodos.map((todo) => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.complete}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
+        )}
+      </TodoContext.Consumer>
+
       <CreateTodoButton />
     </>
   );
